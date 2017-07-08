a86092841f6b42272a07791f0dc24fde97a938d9
Jindrich Mynarz <mynarzjindrich@gmail.com>
2017-07-08 20:31:09 +0200

    No tests so far

--- test/sparql_to_tensor/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns sparql-to-tensor.core-test
-  (:require [clojure.test :refer :all]
-            [sparql-to-tensor.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
